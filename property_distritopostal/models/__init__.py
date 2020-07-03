0490a0bf5d31b433420e4cdce3faeab6a50241db
Informatica Arelux <informatica@arelux.com>
2020-07-03 09:41:08 +0200

    # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).

--- property_distritopostal/models/__init__.py
+++ property_distritopostal/models/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from . import distritopostal_state
 from . import distritopostal_municipality
 from . import distritopostal_postalcode
--- property_sedecatastro/models/sedecatastro_municipio.py
+++ property_sedecatastro/models/sedecatastro_municipio.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 #https://ovc.catastro.meh.es/ovcservweb/OVCSWLocalizacionRC/OVCCallejero.asmx?op=ConsultaMunicipio
 from odoo import api, fields, models
 
