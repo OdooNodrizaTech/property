dbcb9d52cf2aa48fe537406897f89ec9b3f1fbfd
Informatica Arelux <informatica@arelux.com>
2020-07-03 09:38:22 +0200

    # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).

--- property_property/models/__init__.py
+++ property_property/models/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 import distritopostal_municipality
 import distritopostal_state
 import distritopostal_way
--- property_sedecatastro/models/__init__.py
+++ property_sedecatastro/models/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 import sedecatastro_provincia
 import sedecatastro_municipio
 import sedecatastro_numero
